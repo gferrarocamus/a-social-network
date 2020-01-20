9f55edfa340ecd442f1fd0a5e58b3d6682c8c98a
giulianaferraro <giulianaferrarocamus@gmail.com>
2020-01-20 20:33:58 +0100

    Allow image posts

--- app/assets/javascripts/posts.coffee
+++ app/assets/javascripts/posts.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "change", "#post_picture", ->
+  size_in_megabytes = this.files[0].size/1024/1024;
+  if size_in_megabytes > 1
+    document.getElementById("error-explanation").classList.remove("d-none");
+    this.value = ""
+    return
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  mount_uploader :picture, PictureUploader
 
   validates :content, presence: true
+  validate :picture_size
 
   scope :date_sorted, -> { order(created_at: :desc) }
 
@@ -18,4 +20,12 @@ class Post < ApplicationRecord
             comments: [:user]
           )
         }
+  
+  private
+
+  def picture_size
+    if picture.size > 1.megabytes
+      errors.add(:picture, "should be less than 1MB")
+    end
+  end
 end
